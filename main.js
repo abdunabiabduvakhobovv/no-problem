9cebc8b252972ac6bfae1a84a286561bc08390b5
abdunabiabduvakhobovv <abdunabiabduvakhobov60@gmail.com>
2025-02-10 16:25:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let name = prompt("ismingizni yozing")
+let username = prompt("familyangizni yozing")
+let old = prompt("yoshingizni yozing")
+let textbook = prompt("yahshi korgan faningizni yozing")
+let map = prompt("yashash joyingizni yozing")
+console.log(name);
+console.log(username);
+console.log(old);
+console.log(textbook);
+console.log(map);
+
+
+
+
+
